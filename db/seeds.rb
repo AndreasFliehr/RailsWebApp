16586d428fd9cbaa305a24055169d38311ad7f3c
Andreas Fliehr <afliehr@gmx.de>
2017-05-24 01:00:45 +0200

    Before_lesson_3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: "Das ist eine Aufgabe")
+Task.create(title: "Das ist die zweite Aufgabe")
+Task.first.comments.create(body: "Das ist ein Kommentar zur ersten Aufgabe")
+Task.last.comments.create(body: "Das ist ein Kommentar zur letzten Aufgabe")
